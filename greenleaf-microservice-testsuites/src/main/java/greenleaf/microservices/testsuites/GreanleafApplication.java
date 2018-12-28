9d979e75c2053fb0674d40101015e0dbc5336916
Alex <alexander.frommelt@googlemail.com>
2018-12-28 19:33:29 +0100

    added reporting corrected some configuration bugs added base test case implementation

---
+++ greenleaf-microservice-testsuites/src/main/java/greenleaf/microservices/testsuites/GreanleafApplication.java
@@ -0,0 +1,13 @@
+package greenleaf.microservices.testsuites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreanleafApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(GreanleafApplication.class, args);
+	}
+
+}
