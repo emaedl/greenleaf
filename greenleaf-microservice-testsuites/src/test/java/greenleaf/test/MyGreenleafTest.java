c20a0e8e88d443f919b354f9ad9f2525b9846650
emaedl <alexander.frommelt@googlemail.com>
2018-12-28 20:05:47 +0100

    added .springbeans to .gitignore

---
+++ greenleaf-microservice-testsuites/src/test/java/greenleaf/test/MyGreenleafTest.java
@@ -0,0 +1,14 @@
+package greenleaf.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyGreenleafTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		Assert.assertTrue(true);
+	}
+
+}
